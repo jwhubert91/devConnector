f8712c92fffa20dd5ac92baf51c7dc1b172fe443
james w hubert <jwhubert91@gmail.com>
2020-04-14 20:49:38 -0400

    Modify route to get data from github profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -330,14 +330,14 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 router.get('/github/:username', async (req,res) => {
 	try {
 		const options = {
-			uri: `https://api.github.com/users/${
-				req.params.username
-			}
-			/repos?per_page=5&sort=created:asc&client_id=${config.get(
-				'githubClientID'
-			)}&client_secret=${config.get('githubSecret')}`,
+			uri: encodeURI(
+				`https://api.github.com/users/${req.params.username}/repos?per_page&sort=created:asc`
+			),
 			method: 'GET',
-			headers: {'user-agent':'node.js'}
+			headers: {
+				'user-agent':'node.js',
+				Authorization: `token ${config.get('githubPersonalAccessToken')}`
+			}
 		};
 
 		request(options, (error, response, body) => {
